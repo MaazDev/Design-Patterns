5887cde35e471e32b6db5daba49cc78389bb3098
Maaz <lecomptevip@gmail.com>
2020-05-15 00:01:30 +0200

    Added Null Pattern demo

---
+++ src/nullPatern.ts
@@ -0,0 +1,37 @@
+class User {
+	constructor(
+		public name,
+		public age
+	) {
+	}
+
+	hasAccess() {
+		return this.name === 'Jean';
+	}
+}
+
+class NullUser {
+	constructor(
+		public name = 'Guest',
+		public age = 'Guest ages'
+	) {
+	}
+
+	hasAccess() {
+		return false;
+	}
+}
+
+const users = [
+	new User('Jean', 3),
+	new User('pierre', 6)
+];
+
+function getUser(userName) {
+	return users.find(user => user.name === userName) || new NullUser();
+}
+
+getUser('d');
+getUser('Jean');
+
+
